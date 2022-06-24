6dd38f576137b72ac5c871c9ae40c024ad0e856c
Amadeus979 <xuruiyuan20000828@163.com>
2022-06-25 04:43:43 +0800

    xjbx

---
+++ src/ir/Use.java
@@ -0,0 +1,44 @@
+package ir;
+
+import util.ILinkNode;
+
+import java.util.Objects;
+
+public class Use extends ILinkNode {
+    private Instr user;
+    private Value used;
+    private int idx;
+
+    public Use(){}
+
+    public Use(Instr user, Value used, int idx) {
+        this.user = user;
+        this.used = used;
+        this.idx = idx;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Use use = (Use) o;
+        return idx == use.idx && used.equals(use.used) && user.equals(use.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(used, user, idx);
+    }
+
+    public Value getUsed() {
+        return used;
+    }
+
+    public Instr getUser() {
+        return user;
+    }
+
+    public int getIdx() {
+        return idx;
+    }
+}
---
+++ src/ir/Value.java
@@ -0,0 +1,27 @@
+package ir;
+
+import util.ILinkNode;
+
+public class Value extends ILinkNode {
+    public static final String GLOBAL_PREFIX = "@";
+    public static final String LOCAL_PREFIX = "%";
+    public static final String BB_NAME_PREFIX = "b";
+    public static final String GLOBAL_NAME_PREFIX = "g";
+    public static final String LOCAL_NAME_PREFIX = "v";
+    private static int BB_COUNT = 0;
+    private static int LOCAL_COUNT = 0;
+    private static int GLOBAL_COUNT = 0;
+    public String prefix = this instanceof GlobalVar ? GLOBAL_PREFIX : LOCAL_PREFIX;
+    public String name = this instanceof BasicBlock ? BB_NAME_PREFIX + BB_COUNT++ : (this instanceof GlobalVar ? GLOBAL_NAME_PREFIX + GLOBAL_COUNT++ : LOCAL_NAME_PREFIX + LOCAL_COUNT++);
+
+    private Use begin = new Use();
+    private Use end = begin;
+
+    public Value() {
+    }
+
+    public void insertAtEnd(Use use) {
+        end.insertAfter(use);
+        end = use;
+    }
+}

c53285a70d5563caad7f5fce40520bdbb4f34057
Amadeus979 <xuruiyuan20000828@163.com>
2022-07-04 02:35:52 +0800

    debug fparam output

--- src/mir/Function.java
+++ src/mir/Function.java
@@ -32,7 +32,7 @@ public class Function extends Value{
 
         @Override
         public String toString(){
-            return type.toString() + " " + name;
+            return type.toString() + " " + getName();
         }
 
     }

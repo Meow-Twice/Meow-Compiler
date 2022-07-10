cc71e9ce18a49e2371f067ef119118f7bdafbd18
刘传 <925992278@qq.com>
2022-07-10 16:51:32 +0800

    LCSSA-init

---
+++ src/midend/BranchLift.java
@@ -0,0 +1,22 @@
+package midend;
+
+import mir.Function;
+
+public class BranchLift {
+
+    public BranchLift() {
+
+    }
+
+    public void Run() {
+
+    }
+
+    private void branchLift() {
+
+    }
+
+    private void branchLiftForFunc(Function function) {
+
+    }
+}

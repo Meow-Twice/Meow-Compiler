b941e397de406d4316a6b07036a51057c4c38d91
刘传 <925992278@qq.com>
2022-07-23 22:33:24 +0800

    loop_fuse_init

--- src/midend/LoopFuse.java
+++ src/midend/LoopFuse.java
@@ -1,8 +1,11 @@
 package midend;
 
 import mir.Function;
+import mir.Loop;
+import mir.Value;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class LoopFuse {
 
@@ -19,7 +22,45 @@ public class LoopFuse {
     }
 
     private void loopFuseForFunc(Function function) {
+        Loop loop = function.getBeginBB().getLoop();
+        solve(loop);
+    }
 
+    private void solve(Loop loop) {
+        fuseChildrenLoopInLoop(loop);
+        for (Loop next: loop.getChildrenLoops()) {
+            solve(next);
+        }
+    }
+
+    private void fuseChildrenLoopInLoop(Loop loop) {
+        HashSet<Loop> ret = new HashSet<>();
+        for (Loop temp: loop.getChildrenLoops()) {
+            mergeInto(temp, ret);
+        }
+    }
+
+    private void mergeInto(Loop loop, HashSet<Loop> loops) {
+        if (!loop.isIdcSet()) {
+            loops.add(loop);
+            return;
+        }
+        for (Loop temp: loops) {
+            if (!temp.isIdcSet()) {
+                continue;
+            }
+            if (tryMergeAtoB(loop, temp)) {
+                return;
+            }
+        }
+        loops.add(loop);
+    }
+
+    //成功merge则返回true
+    private boolean tryMergeAtoB(Loop A, Loop B) {
+        Value idcInitA = A.getIdcInit();
+
+        return true;
     }
 
 }

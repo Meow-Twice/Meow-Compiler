47fb6a3c8f5dd99ead62014d406372c7fd502812
Amadeus979 <xuruiyuan20000828@163.com>
2022-08-17 02:52:31 +0800

    stable=true和global_bss=true

--- src/util/CenterControl.java
+++ src/util/CenterControl.java
@@ -3,13 +3,13 @@ package util;
 public class CenterControl {
     public static final boolean _FAST_REG_ALLOCATE = false;
     public static final boolean _IMM_TRY = false;
-    public static final boolean STABLE = false;
+    public static final boolean STABLE = true;
     public static boolean _ONLY_FRONTEND = false;
     public static final boolean _cutLiveNessShortest = false;
     public static final boolean _FixStackWithPeepHole = true;
     public static final boolean _AGGRESSIVE_ADD_MOD_OPT = false;
     public static final boolean _OPEN_PARALLEL = false;
-    public static final boolean _GLOBAL_BSS = false;
+    public static final boolean _GLOBAL_BSS = true;
     public static boolean AlreadyBackend = false;
 
     public static final String _TAG = "1";

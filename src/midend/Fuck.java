3c7e076aec84da4ccb94bff29cfa7d591ef2da4b
Amadeus979 <xuruiyuan20000828@163.com>
2022-08-20 10:43:53 +0800

    unroll-1w

---
+++ src/midend/Fuck.java
@@ -0,0 +1,29 @@
+// package midend;
+//
+// import frontend.semantic.Initial;
+// import mir.Function;
+// import mir.GlobalVal;
+//
+// import java.util.ArrayList;
+// import java.util.HashMap;
+//
+// public class Fuck {
+//
+//     private ArrayList<Function> functions;
+//     private HashMap<GlobalVal.GlobalValue, Initial> globalValues;
+//
+//     public Fuck(ArrayList<Function> functions, HashMap<GlobalVal.GlobalValue, Initial> globalValues) {
+//         this.functions = functions;
+//         this.globalValues = globalValues;
+//         Run();
+//     }
+//
+//     public void Run() {
+//         for (GlobalVal.GlobalValue globalVal: globalValues.keySet()) {
+//
+//         }
+//         for (Function function: functions) {
+//             for ()
+//         }
+//     }
+// }
